46780bb66a61a6c3d1fa2cabbfc8f7056b774bcc
dallaway <dallaway@37c72bd0-721e-0410-aae7-03fd94bbba03>
2006-10-03 08:36:44 +0000

    removed bad test of comapring charts by bytes, as bytes differ on different architectures.

--- trunk/junit/com/googlecode/jchav/chart/MinMeanMaxChartTest.java
+++ trunk/junit/com/googlecode/jchav/chart/MinMeanMaxChartTest.java
@@ -16,171 +16,80 @@
  */
 package com.googlecode.jchav.chart;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.SortedSet;
-
-import org.junit.Before;
-import org.junit.Test;
 
 import com.googlecode.jchav.TestData;
 import com.googlecode.jchav.data.Measurement;
 
 /**
- * Test of creating a chart. 
+ * Test of creating a chart.
  * 
- * This test uses a byte[] comparison to ensure that created 
- * charts match expected graph data.  Of course, if we change
- * the layout of a chart, these tests will break, so regenerate
- * the data by running the <code>main()</code> of this class.
+ * This is not a unit test, but instead is a main that allows you to produce an
+ * example graph for eye-balling.
  * 
- *
+ * 
  * @author $LastChangedBy$
- * @version $LastChangedDate$ $LastChangedRevision$
+ * @version $LastChangedDate$
+ *          $LastChangedRevision$
  */
 public class MinMeanMaxChartTest
 {
 
     /** The object under test. */
     private MinMeanMaxChart chart;
-    
-    /**
-     * For v4 junit tests to run through ant we currently need the adapter.
-     * @return suite of tests.
-     */
-    public static junit.framework.Test suite() 
-    {
-        return new junit.framework.JUnit4TestAdapter(MinMeanMaxChartTest.class);
-    }
-    
+
     /**
      * Set up common test data.
      */
-    @Before public void setUp()
+    public void setUp()
     {
         // A page name we know exists in the dummy data.
-        final String pageId = "Summary"; 
-        
+        final String pageId = "Summary";
+
         // Data for the page:
         final DummyPageData pageData = new DummyPageData();
         final SortedSet<Measurement> data = pageData.getMeasurements(pageId);
-        
+
         // Sanity checks on the test data:
-        assertNotNull(data);
-        assertEquals(4, data.size());
+        // assertNotNull(data);
+        // assertEquals(4, data.size());
 
-         chart = new MinMeanMaxChart(pageId, data);
-         chart.setHeight(400);
-         chart.setWidth(600);
-         chart.setThumbnailScale(0.5d);
+        chart = new MinMeanMaxChart(pageId, data);
+        chart.setHeight(400);
+        chart.setWidth(600);
+        chart.setThumbnailScale(0.5d);
     }
-    
-    /**
-     * Helper to comapre the contents of a test data file and a
-     *  byte array, failing if they do not exactly match.
-     *  
-     * @param filename the chart test data file to compare against.
-     * @param actual the actualy bytes created.
-     * 
-     * @throws IOException if there was a problem reading the data.
-     */
-    private void assertFileAndBytesTheSame(final String filename, final ByteArrayOutputStream actual)
-        throws IOException
-    {
-        File file = TestData.getTestDataFile("chartoutput", filename);
-        assertTrue("Missing file: "+filename, file.exists());
-        
-        // Read the file:
-        ByteArrayOutputStream expected = new ByteArrayOutputStream();
-        FileInputStream in = new FileInputStream(file);
-        int bufsize = 1024;
-        byte[] buffer = new byte[bufsize];
-        int n = 0;
-        while ( (n=in.read(buffer)) != -1)
-        {
-            expected.write(buffer,0,n);
-        }
-        in.close();
-        expected.close();
-       
-        // Compare the bytes:
-        byte[] actualBytes = actual.toByteArray();
-        byte[] expectedBytes = expected.toByteArray();
-        
-        assertEquals(expectedBytes.length, actualBytes.length);
-        
-        for(int i=0; i<expectedBytes.length; i++)
-        {
-            assertEquals("Bytes differ at position "+i, expectedBytes[i], actualBytes[i]);
-        }
-        
-    }
-    
-    /**
-     * Test that we can create a PNG graph from the Dummy data.
-     * 
-     * @throws IOException if the test fails unexpectedly.
-     */
-    @Test public void canCreateAChart() throws IOException
-    {
-
-        final ByteArrayOutputStream out = new ByteArrayOutputStream();
-        chart.write(out);
-        out.close();
-       
-        assertFileAndBytesTheSame("summary.png", out);  
-    }
-    
 
     /**
-     * Test of creating a thumbnail.
+     * Create an example thumbnail and full-size chart.
      * 
-     * @throws IOException if the test fails unexpectedly.
-     */
-    @Test public void canCreateThumbnail() throws IOException
-    {
-
-        final ByteArrayOutputStream out = new ByteArrayOutputStream();
-        chart.writeThumbnail(out);
-        out.close();
-       
-        assertFileAndBytesTheSame("summary_thumb.png", out);
-    }
-    
-    /**
-     * Create an example thumbnail and full-size chart to
-     * use as input to these unit tests.
+     * @param args
+     *            ignored.
      * 
-     * @param args ignored.
-     * 
-     * @throws IOException if it was not possible to create the charts.
+     * @throws IOException
+     *             if it was not possible to create the charts.
      */
     public static void main(final String[] args) throws IOException
     {
-       
+
         MinMeanMaxChartTest test = new MinMeanMaxChartTest();
         test.setUp();
-        
+
         File outDir = TestData.getTestDataFile("chartoutput");
-       
+
         final FileOutputStream fullsize = new FileOutputStream(new File(outDir, "summary.png"));
         test.chart.write(fullsize);
         fullsize.close();
-        
+
         final FileOutputStream thumb = new FileOutputStream(new File(outDir, "summary_thumb.png"));
         test.chart.writeThumbnail(thumb);
         thumb.close();
-        
-        System.out.println("Test data created in: "+outDir.getAbsolutePath());
-        
+
+        System.out.println("Test data created in: " + outDir.getAbsolutePath());
+
     }
-    
-    
+
 }
