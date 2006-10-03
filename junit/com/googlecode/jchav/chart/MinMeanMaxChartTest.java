cd9c9476849ddc04f1bbe6ced584d6939c982829
Richard Dallaway <richard@dallaway.com>
2006-10-03 07:30:38 +0000

    Added a helper for locating the testdata directory.

--- junit/com/googlecode/jchav/chart/MinMeanMaxChartTest.java
+++ junit/com/googlecode/jchav/chart/MinMeanMaxChartTest.java
@@ -18,8 +18,11 @@ package com.googlecode.jchav.chart;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.SortedSet;
@@ -27,10 +30,17 @@ import java.util.SortedSet;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.googlecode.jchav.TestData;
 import com.googlecode.jchav.data.Measurement;
 
 /**
  * Test of creating a chart. 
+ * 
+ * This test uses a byte[] comparison to ensure that created 
+ * charts match expected graph data.  Of course, if we change
+ * the layout of a chart, these tests will break, so regenerate
+ * the data by running the <code>main()</code> of this class.
+ * 
  *
  * @author $LastChangedBy$
  * @version $LastChangedDate$ $LastChangedRevision$
@@ -67,6 +77,50 @@ public class MinMeanMaxChartTest
         assertEquals(4, data.size());
 
          chart = new MinMeanMaxChart(pageId, data);
+         chart.setHeight(400);
+         chart.setWidth(600);
+         chart.setThumbnailScale(0.5d);
+    }
+    
+    /**
+     * Helper to comapre the contents of a test data file and a
+     *  byte array, failing if they do not exactly match.
+     *  
+     * @param filename the chart test data file to compare against.
+     * @param actual the actualy bytes created.
+     * 
+     * @throws IOException if there was a problem reading the data.
+     */
+    private void assertEquals(final String filename, final ByteArrayOutputStream actual)
+        throws IOException
+    {
+        File file = TestData.getTestDataFile("chartoutput", filename);
+        assertTrue("Missing file: "+filename, file.exists());
+        
+        // Read the file:
+        ByteArrayOutputStream expected = new ByteArrayOutputStream();
+        FileInputStream in = new FileInputStream(file);
+        int bufsize = 1024;
+        byte[] buffer = new byte[bufsize];
+        int n = 0;
+        while ( (n=in.read(buffer)) != -1)
+        {
+            expected.write(buffer,0,n);
+        }
+        in.close();
+        expected.close();
+       
+        // Compare the bytes:
+        byte[] actualBytes = actual.toByteArray();
+        byte[] expectedBytes = expected.toByteArray();
+        
+        assertEquals(expectedBytes.length, actualBytes.length);
+        
+        for(int i=0; i<expectedBytes.length; i++)
+        {
+            assertEquals("Bytes differ at position "+i, expectedBytes[i], actualBytes[i]);
+        }
+        
     }
     
     /**
@@ -76,20 +130,12 @@ public class MinMeanMaxChartTest
      */
     @Test public void canCreateAChart() throws IOException
     {
-        
-        final File file = new File("jchav-summary.png");
-        final FileOutputStream out = new FileOutputStream(file);
-       
-        //final ByteArrayOutputStream out = new ByteArrayOutputStream();
-        
-        chart.setHeight(400);
-        chart.setWidth(600);
+
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
         chart.write(out);
-        
         out.close();
-        
-        System.out.println(file.getAbsolutePath());
-        
+       
+        assertEquals("summary.png", out);  
     }
     
 
@@ -101,19 +147,38 @@ public class MinMeanMaxChartTest
     @Test public void canCreateThumbnail() throws IOException
     {
 
-        final File file = new File("jchav-summary-thumb.png");
-        final FileOutputStream out = new FileOutputStream(file);
-       
-        //final ByteArrayOutputStream out = new ByteArrayOutputStream();
-        
-        chart.setHeight(400);
-        chart.setWidth(600);
-        chart.setThumbnailScale(0.5d);
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
         chart.writeThumbnail(out);
-        
         out.close();
+       
+        assertEquals("summary_thumb.png", out);
+    }
+    
+    /**
+     * Create an example thumbnail and full-size chart to
+     * use as input to these unit tests.
+     * 
+     * @param args ignored.
+     * 
+     * @throws IOException if it was not possible to create the charts.
+     */
+    public static void main(final String[] args) throws IOException
+    {
+       
+        MinMeanMaxChartTest test = new MinMeanMaxChartTest();
+        test.setUp();
         
-        System.out.println(file.getAbsolutePath());
+        File outDir = TestData.getTestDataFile("chartoutput");
+       
+        final FileOutputStream fullsize = new FileOutputStream(new File(outDir, "summary.png"));
+        test.chart.write(fullsize);
+        fullsize.close();
+        
+        final FileOutputStream thumb = new FileOutputStream(new File(outDir, "summary_thumb.png"));
+        test.chart.writeThumbnail(thumb);
+        thumb.close();
+        
+        System.out.println("Test data created in: "+outDir.getAbsolutePath());
         
     }
     
